f7521974166ea2c25f53de74b5d92b903d971417
ShreyasAyyengar <shreyas.ayyengar@gmail.com>
2024-10-20 12:09:35 -0700

    feat: extension functions.

--- src/main/kotlin/host/carbon/event/util/Extn.kt
+++ src/main/kotlin/host/carbon/event/util/Extn.kt
@@ -1,5 +1,18 @@
 package host.carbon.event.util
 
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
+import java.util.UUID
 
 fun String.asComponent() = LegacyComponentSerializer.legacyAmpersand().deserialize(this)
+
+fun String.colourise(): String = ChatColor.translateAlternateColorCodes('&', this)
+
+fun UUID.toBukkitPlayer(): Player? {
+    Bukkit.getPlayer(this)?.let {
+        return it
+    }
+    return null
+}
