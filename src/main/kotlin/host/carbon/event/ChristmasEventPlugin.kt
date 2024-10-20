de3c7ab55bdc87cc2befc5474b60911fffee5ca8
ShreyasAyyengar <shreyas.ayyengar@gmail.com>
2024-10-20 12:04:12 -0700

    feat: flush main class

--- src/main/kotlin/host/carbon/event/ChristmasEventPlugin.kt
+++ src/main/kotlin/host/carbon/event/ChristmasEventPlugin.kt
@@ -1,34 +1,79 @@
 package host.carbon.event
 
+import com.github.retrooper.packetevents.PacketEvents
+import gg.flyte.twilight.twilight
+import host.carbon.event.listeners.HousekeepingEventListener
+import host.carbon.event.minigame.engine.EventController
+import host.carbon.event.npc.WorldNPC
+import host.carbon.event.util.Util
+import io.github.retrooper.packetevents.factory.spigot.SpigotPacketEventsBuilder
 import org.bukkit.Bukkit
+import org.bukkit.Location
+import org.bukkit.World
 import org.bukkit.plugin.java.JavaPlugin
 import revxrsal.commands.bukkit.BukkitLamp
-import java.util.stream.Stream
+import java.util.UUID
 
 class ChristmasEventPlugin : JavaPlugin() {
+    lateinit var serverWorld: World;
+    lateinit var lobbySpawn: Location
+    var cameraPlayer: UUID = UUID.fromString("a008c892-e7e1-48e1-8235-8aa389318b7a") // "devous" | Josh
+    var eventController: EventController = EventController()
+
+    companion object {
+        fun getInstance(): ChristmasEventPlugin = getPlugin(ChristmasEventPlugin::class.java)
+    }
+
+    override fun onLoad() {
+        PacketEvents.setAPI(SpigotPacketEventsBuilder.build(this))
+        PacketEvents.getAPI().load()
+    }
 
     override fun onEnable() {
         Bukkit.getLogger().info("Christmas Event Plugin has been enabled!")
 
+        createConfig();
         registerCommands();
         registerEvents();
+        registerPacketAPI()
+        loadContributorNPCs();
+        fixData()
+
+        twilight(this)
     }
 
     override fun onDisable() {
     }
 
+    private fun createConfig() {
+        config.options().configuration();
+        saveDefaultConfig()
+    }
 
     private fun registerCommands() {
         val lamp = BukkitLamp.builder(this).build()
     }
 
     private fun registerEvents() {
-        Stream.of<>(
-
-        ).forEach(
+        HousekeepingEventListener()
     }
 
-    fun getInstance(): ChristmasEventPlugin {
-        return JavaPlugin.getPlugin(ChristmasEventPlugin::class.java)
+    private fun registerPacketAPI() {
+        PacketEvents.getAPI().init();
+    }
+
+    private fun loadContributorNPCs() {
+        for (contributor in Util.getEventContributors()) {
+            val name = contributor.key
+            val contribution = contributor.value.first
+            val location = contributor.value.second
+
+            WorldNPC.createFromName(name, location)
+        }
+    }
+
+    private fun fixData() {
+        serverWorld = Bukkit.getWorld("world")!!
+        lobbySpawn = Util.getLobbySpawnLocation()
     }
 }
