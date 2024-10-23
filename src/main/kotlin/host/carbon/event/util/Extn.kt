ce9ae3e5ab94e2fc6d563ee68e299cf5056cdb01
ShreyasAyyengar <shreyas.ayyengar@gmail.com>
2024-10-22 17:42:32 -0700

    feat: add colourise method to work with legacy ampersand text. (components aren't good for everything, okay?)

--- src/main/kotlin/host/carbon/event/util/Extn.kt
+++ src/main/kotlin/host/carbon/event/util/Extn.kt
@@ -1,5 +1,6 @@
 package host.carbon.event.util
 
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
@@ -8,7 +9,28 @@ import java.util.UUID
 
 fun String.asComponent() = LegacyComponentSerializer.legacyAmpersand().deserialize(this)
 
-fun String.colourise(): String = ChatColor.translateAlternateColorCodes('&', this)
+fun String.colourise(): String {
+    val pattern = Regex("#[a-fA-F0-9]{6}")
+    var message = this
+
+    pattern.findAll(this).forEach { matchResult ->
+        val hexCode = matchResult.value
+        val replaceSharp = hexCode.replace("#", "x")
+
+        val builder = StringBuilder()
+        replaceSharp.forEach { c ->
+            builder.append("&").append(c)
+        }
+
+        message = message.replace(hexCode, builder.toString())
+    }
+
+    return ChatColor.translateAlternateColorCodes('&', message)
+}
+
+fun Component.toLegacyString(): String {
+    return LegacyComponentSerializer.legacyAmpersand().serialize(this)
+}
 
 fun UUID.toBukkitPlayer(): Player? {
     Bukkit.getPlayer(this)?.let {
