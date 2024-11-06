499b59ffe46c1fc34f75b39574efb6b54f5df026
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2024-11-06 13:16:17 -0800

    fix: missing import
    Signed-off-by: Shreyas Ayyengar <shreyas.ayyengar@gmail.com>

--- src/main/kotlin/gg/flyte/christmas/minigame/engine/GameConfig.kt
+++ src/main/kotlin/gg/flyte/christmas/minigame/engine/GameConfig.kt
@@ -3,6 +3,7 @@ package gg.flyte.christmas.minigame.engine
 import gg.flyte.christmas.minigame.games.BlockParty
 import gg.flyte.christmas.minigame.games.KingHill
 import gg.flyte.christmas.minigame.games.MusicalMinecarts
+import gg.flyte.christmas.minigame.games.PaintWars
 import gg.flyte.christmas.minigame.world.MapRegion
 import gg.flyte.christmas.minigame.world.MapSinglePoint
 import net.kyori.adventure.text.Component
