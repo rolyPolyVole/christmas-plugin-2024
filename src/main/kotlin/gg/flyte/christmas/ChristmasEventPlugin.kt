6b61da2cec6c2cb75f0e12e6e9855965e972871d
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2024-11-08 21:42:43 -0800

    refactor: remove unused import
    Signed-off-by: Shreyas Ayyengar <shreyas.ayyengar@gmail.com>

--- src/main/kotlin/gg/flyte/christmas/ChristmasEventPlugin.kt
+++ src/main/kotlin/gg/flyte/christmas/ChristmasEventPlugin.kt
@@ -23,7 +23,6 @@ import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Bukkit
 import org.bukkit.Color
 import org.bukkit.GameRule
-import org.bukkit.Location
 import org.bukkit.World
 import org.bukkit.entity.Display
 import org.bukkit.entity.TextDisplay
