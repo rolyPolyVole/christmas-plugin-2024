948371ca5c5f3abae2031bb4334b48f3adb887f7
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2024-12-01 22:15:50 -0800

    feat: add handleDonation method stub.
    Signed-off-by: Shreyas Ayyengar <shreyas.ayyengar@gmail.com>

--- src/main/kotlin/gg/flyte/christmas/minigame/games/Avalanche.kt
+++ src/main/kotlin/gg/flyte/christmas/minigame/games/Avalanche.kt
@@ -4,6 +4,7 @@ import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEn
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityRotation
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSetPassengers
 import gg.flyte.christmas.ChristmasEventPlugin
+import gg.flyte.christmas.donation.DonationTier
 import gg.flyte.christmas.minigame.engine.EventMiniGame
 import gg.flyte.christmas.minigame.engine.GameConfig
 import gg.flyte.christmas.minigame.engine.PlayerType
@@ -339,6 +340,14 @@ class Avalanche : EventMiniGame(GameConfig.AVALANCHE) {
             if (player.location.blockY < 104) {
                 if (remainingPlayers().contains(player)) eliminate(player, EliminationReason.ELIMINATED)
             }
+        }
+    }
+
+    override fun handleDonation(tier: DonationTier) {
+        when (tier) {
+            DonationTier.LOW -> TODO()
+            DonationTier.MEDIUM -> TODO()
+            DonationTier.HIGH -> TODO()
         }
     }
 }
