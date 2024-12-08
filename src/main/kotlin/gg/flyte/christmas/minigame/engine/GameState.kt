67fd1c73622a2aebc0eb5d92d46ca715606646c1
Shreyas Ayyengar <shreyas.ayyengar@gmail.com>
2024-12-08 11:19:22 -0800

    style: add whitespace
    Signed-off-by: Shreyas Ayyengar <shreyas.ayyengar@gmail.com>

--- src/main/kotlin/gg/flyte/christmas/minigame/engine/GameState.kt
+++ src/main/kotlin/gg/flyte/christmas/minigame/engine/GameState.kt
@@ -5,14 +5,17 @@ enum class GameState {
      * The [GameConfig] (game type) has been selected, but has no requests to start.
      */
     IDLE,
+
     /**
      * The game is waiting for players to join.
      */
     WAITING_FOR_PLAYERS,
+
     /**
      * The game is counting down to start.
      */
     COUNTDOWN,
+
     /**
      * The game is running.
      */
